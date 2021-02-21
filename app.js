e5152e89dfdbdd6885a7615bbfb96440ca0bab7f
JosephASandoval <josephasandoval17@gmail.com>
2021-02-21 12:21:29 -0800

    finished mern setup

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-  res.send("Hello aA!");
+  res.send("Hello world!");
 });
 
 const port = process.env.PORT || 5000;
