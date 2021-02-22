b060eb1d6d0ce97b55c07672e6b0c52cd2de7186
JosephASandoval <josephasandoval17@gmail.com>
2021-02-21 23:18:22 -0800

    updated

--- app.js
+++ app.js
@@ -3,15 +3,43 @@ const app = express();
 const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI;
 
+const User = require("./models/User");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
 mongoose
-  .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
+  .connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  const user = new User({
+    handle: "jim",
+    email: "jim@gmail.com",
+    password: "password",
+  });
+  user.save();
+  res.send("Hello World!!");
 });
+// request object and response object
+
+app.use(passport.initialize());
+require("./config/passport")(passport);
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
